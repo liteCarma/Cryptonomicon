ca3a9d4df089a3d1c01f31f9d244cfdeaf583a40
Denis <litecarma@gmail.com>
2022-03-09 20:54:43 +0700

    feature (App.vue, api.js) add suggested tickers for input

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup>
 import {
-  ref, onMounted, onBeforeUnmount, reactive, watch, nextTick,
+  ref, onMounted, onBeforeUnmount, reactive, watch, nextTick, computed,
 } from 'vue';
 import Client from './assets/api.js';
 
 const client = new Client();
 
 const tickerData = reactive({
-  ticker: 'BTC',
+  ticker: '',
   selectedTicker: null,
   tickerList: [],
   showTickerError: false,
 });
+
+const suggestedCoins = computed(() => client.getSuggested(tickerData.ticker, 4));
 
 const graphContainer = ref(null);
 const BAR_WIDTH = 38;
@@ -34,6 +36,10 @@ watch(() => tickerData.selectedTicker, () => {
   nextTick(calculateGraphVisibleBars);
 });
 
+watch(() => tickerData.ticker, () => {
+  tickerData.showTickerError = false;
+});
+
 window.addEventListener('resize', calculateGraphVisibleBars);
 
 onBeforeUnmount(() => {
@@ -48,8 +54,7 @@ function addTicker() {
     return;
   }
 
-  tickerData.showTickerError = false;
-
+  tickerData.ticker = '';
   const newTicker = reactive({
     name: ticker,
     price: '-',
@@ -105,6 +110,11 @@ function getVisibleBars() {
   }
   return graph.bars;
 }
+
+function suggestedClick(ticker) {
+  tickerData.ticker = ticker.toUpperCase();
+  nextTick(addTicker);
+}
 </script>
 
 <template>
@@ -145,7 +155,7 @@ function getVisibleBars() {
             <div class="mt-1 relative rounded-md shadow-md">
               <input
                 id="wallet"
-                v-model="tickerData.ticker"
+                v-model.trim="tickerData.ticker"
                 type="text"
                 name="wallet"
                 class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
@@ -153,9 +163,17 @@ function getVisibleBars() {
                 @keyup.enter="addTicker"
               >
             </div>
-            <div class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
-              <span class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
-                BTC
+            <div
+              v-if="suggestedCoins.length > 0"
+              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
+            >
+              <span
+                v-for="(ticker, index) of suggestedCoins"
+                :key="index"
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+                @click="suggestedClick(ticker)"
+              >
+                {{ ticker }}
               </span>
             </div>
             <div
--- src/assets/api.js
+++ src/assets/api.js
@@ -6,6 +6,12 @@ export default class Client {
     this.events = new EventEmitter();
     this.apiKey = apiKey || config.apiKey;
     this.subscribers = new Map();
+    this.coinList = {
+      list: [],
+      map: {},
+      lastSearchIndex: 0,
+    };
+    this.getcoinList();
     this.connect();
   }
 
@@ -57,5 +63,44 @@ export default class Client {
     if (listener) {
       this.events.removeListener('newPrice', listener);
     }
+  }
+
+  getcoinList() {
+    return fetch(`https://min-api.cryptocompare.com/data/all/coinlist?api_key=${this.apiKey}&summary=true`)
+      .then((r) => r.json())
+      .then(({ Data }) => {
+        const { coinList } = this;
+        const symbols = [];
+        const fullnames = [];
+        Object.values(Data).forEach(({ Symbol, FullName }) => {
+          const symbol = Symbol.toUpperCase();
+          const fullname = FullName.toUpperCase().replace(/[\u200b]/, '');
+          coinList.map[symbol] = symbol;
+          coinList.map[fullname] = symbol;
+          symbols.push(Symbol);
+          fullnames.push(fullname);
+        });
+
+        coinList.list = [...symbols.sort(), ...fullnames.sort()];
+      })
+      .catch(() => []);
+  }
+
+  getSuggested(str, needSuggested) {
+    if (str.length <= this.coinList.lastSearchIndex) this.coinList.lastSearchIndex = 0;
+    if (!this.coinList.list || !str) return [];
+    const suggested = [];
+    for (let i = this.coinList.lastSearchIndex; i < this.coinList.list.length; i += 1) {
+      const ticker = this.coinList.list[i];
+      if (ticker.startsWith(str.toUpperCase())) {
+        suggested.push(ticker);
+        if (suggested.length === needSuggested) {
+          this.coinList.lastSearchIndex = i;
+          break;
+        }
+      }
+    }
+
+    return suggested.map((name) => this.coinList.map[name]);
   }
 }
